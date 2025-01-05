6f94877a151a6916dde3657920a2e644ade6a7be
ray <ray472809026@hotmail.com>
2025-01-05 11:45:56 +0800

    Installed react and tailwindCSS

---
+++ src/components/react/HomePage/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <>
+      <div>Home</div>
+      <button className='btn btn-primary'>Primary</button>
+    </>
+  )
+}
